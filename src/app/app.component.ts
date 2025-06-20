d31c4db362ff7d072f341d2c4f7aaad4a3c14e0f
Jay-boo <jeanalainpro2@gmail.com>
2025-06-20 17:37:49 +0200

    Logo implementation + basic header and setup structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  template:`
+    <div class="app-container">
+      <header class="app-header">
+        <h1>{{ title }}</h1>
+        <nav>
+          <a routerLink="/gallery" routerLinkActive="active">GalleryComponent</a>
+        </nav>
+      </header>
+
+      <main class="main-content">
+        <router-outlet></router-outlet>
+      </main>
+
+      <footer class="app-footer">
+        <p>&copy; 2025 REC</p>
+      </footer>
+    </div>
+
+  `,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  protected title = 'viewer';
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo:'gallery',
+    pathMatch: 'full',
+  },
+  {
+    path: 'gallery',
+    loadChildren:()=> import('./features/photo-gallery/photo-gallery-module').then(m=> m.PhotoGalleryModule)
+  }
+];
